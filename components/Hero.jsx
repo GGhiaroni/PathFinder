593000f3106ad7398473fdb5b9b8016be93e2ee7
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-25 18:57:29 -0300

    iniciando o projeto

---
+++ components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100">
+      <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
+        <h1>Bem-vindo</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
