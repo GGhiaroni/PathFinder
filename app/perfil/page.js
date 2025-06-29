1b62351fa3cd6667eae4b1f61098ca477d80b768
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-29 12:20:40 -0300

    comecando a desenvolver o formulário para criacao do perfil do usuario com o react form

--- app/perfil/page.js
+++ app/perfil/page.js
@@ -1,8 +1,19 @@
 "use client";
 
 import { UserRound } from "lucide-react";
+import { useForm } from "react-hook-form";
 
 const Perfil = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Dados do Perfil: ", data);
+  };
+
   return (
     <section className="min-h-screen bg-gradient-to-r from-indigo-50 to-fuchsia-50">
       <div className="flex flex-col items-center justify-center py-10 px-8">
@@ -16,9 +27,28 @@ const Perfil = () => {
           Conte-nos sobre suas preferências para criarmos seu roteiro perfeito
         </h3>
       </div>
-      <div className="bg-white mx-4 flex flex-col items-center justify-center rounded-lg py-6">
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white mx-4 flex flex-col items-start justify-start rounded-lg py-6 px-6 gap-6"
+      >
         <h3 className="text-3xl font-bold ">Informações básicas</h3>
-      </div>
+
+        <div className="flex flex-col gap-2 justify-start">
+          <label htmlFor="nome" className="font-medium text-textoPreto">
+            Nome <span className="text-red-700">*</span>
+          </label>
+          <input
+            id="nome"
+            placeholder="Seu nome"
+            className="border border-gray-300 rounded-md px-4 py-2"
+            {...register("nome", { required: "Nome é obrigatório" })}
+          />
+          {errors.nome && (
+            <span className="text-red-500 text-sm">{errors.nome.message}</span>
+          )}
+        </div>
+      </form>
     </section>
   );
 };
