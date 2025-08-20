b14c673c8e2ffcf806a633fed46285040efc973c
GGhiaroni <gghiaronitiziano@gmail.com>
2025-08-20 17:18:50 -0300

    criando a página para visualizar determinado roteiro de uma cidade que está salvo

---
+++ app/api/roteiro/[cidade]/route.js
@@ -0,0 +1,46 @@
+import pool from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  try {
+    const { cidade } = params;
+    const { searchParams } = new URL(request.url);
+    const usuarioId = searchParams.get("usuarioId");
+
+    if (!cidade || !usuarioId) {
+      return NextResponse.json(
+        { error: "Nome da cidade ou ID do usuário não fornecido." },
+        { status: 400 }
+      );
+    }
+
+    const client = await pool.connect();
+
+    const query = `
+        SELECT dados_roteiro, titulo, pais_destino
+        FROM roteiros_salvos
+        WHERE usuario_id = $1 AND titulo ILIKE $2;
+        `;
+
+    const result = await client.query(query, [usuarioId, `%${cidade}%`]);
+
+    client.release();
+
+    if (result.rows.length === 0) {
+      return NextResponse.json(
+        { error: "Roteiro não encontrado." },
+        { status: 404 }
+      );
+    }
+
+    const roteiro = result.rows[0];
+
+    return NextResponse.json(roteiro, { status: 200 });
+  } catch (error) {
+    console.error("Erro ao buscar o roteiro:", error);
+    return NextResponse.json(
+      { error: "Erro interno do servidor." },
+      { status: 500 }
+    );
+  }
+}
